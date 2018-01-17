b2a430ed709dc76f247396aff71c9faa4277a087
Takashi Kawasaki <espresso3389@gmail.com>
2018-01-18 07:42:50 +0900

    First commit.

---
+++ dist.bat
@@ -0,0 +1,45 @@
+@echo off
+
+call :find_exe git
+if ERRORLEVEL 1 goto errend
+call :set_revision
+if ERRORLEVEL 1 goto errend
+call :set_commitid
+if ERRORLEVEL 1 goto errend
+
+call npm version 1.0.%REV%
+
+call npm install
+REM call node_modules/.bin/electron-rebuild -a ia32
+call node_modules/.bin/electron-rebuild -a x64
+call npm run compile
+call npm run dist-win
+
+goto :EOF
+
+rem -------------------------------------------------------------------------
+rem Set REV environment variable using git's commit count.
+rem -------------------------------------------------------------------------
+:set_revision
+for /f %%c in ('git log --oneline ^| find /c /v ""') do set REV=%%c
+if "%REV%"=="" (
+	echo Could not determine source revision.
+	exit /b 1
+)
+echo Revision: %REV%
+goto :EOF
+
+rem -------------------------------------------------------------------------
+rem Set COMMIT environment variable using git's commit count.
+rem -------------------------------------------------------------------------
+:set_commitid
+for /f %%c in ('git log --oneline -1') do set COMMIT=%%c
+goto :EOF
+
+rem -------------------------------------------------------------------------
+rem Determine whether the executable is found or not.
+rem -------------------------------------------------------------------------
+:find_exe
+for /f %%c in ('where %1 2^>NUL') do exit /b 0
+echo Command not found: %1
+exit /b 1
---
+++ src/index.js
@@ -0,0 +1,96 @@
+"use strct";
+
+import "babel-core/register";
+import "babel-polyfill";
+import electron from "electron";
+import ffmpeg from "@ffmpeg-installer/ffmpeg";
+import http from "http";
+import child_process from 'child_process';
+import uuidV4 from 'uuid/v4';
+import windowStateKeeper from "electron-window-state";
+
+console.log(ffmpeg.path, ffmpeg.version);
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const appDir = __dirname + "/../src";
+
+let mainWindow = null;
+
+app.commandLine.appendSwitch('ignore-gpu-blacklist', 'true');
+app.commandLine.appendSwitch('enable-gpu-rasterization', 'true');
+app.commandLine.appendSwitch('enable-zero-copy', 'true');
+app.commandLine.appendSwitch('disable-software-rasterizer', 'true');
+
+// hack for OS X
+app.on("window-all-closed", () => {
+  if (process.platform != "darwin") {
+    app.quit();
+  }
+});
+
+app.on("ready", async () => {
+  let mainWindowState = windowStateKeeper({ defaultWidth: 1200, defaultHeight: 900 });
+  mainWindow = new BrowserWindow({
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+    backgroundColor: 'white', show: false
+  });
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+
+  mainWindowState.manage(mainWindow);
+  mainWindow.loadURL(`file://${appDir}/index.html`);
+  mainWindow.show();
+});
+
+const port = 25641;
+const movWidth = 640;
+const movHeight = 360;
+
+let streams = {};
+const guidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+http.createServer((request, response) => {
+  let possibleGuid = request.url.substr(1).toLowerCase();
+  let movie = guidRegex.test(possibleGuid) ? streams[possibleGuid] : null; // FIXME: not bad, but not enough
+  if (!movie)
+  {
+    response.writeHead(404);
+    return response.end();
+  }
+
+  if (!movie.width) movie.width = movWidth;
+  if (!movie.height) movie.height = movWidth;
+
+  response.writeHead(200, {'Content-Type': 'video/webm'});
+  const proc = child_process.spawn(ffmpeg.path, [
+    '-preset', 'ultrafast', '-tune', 'zerolatency',
+    '-i', movie.filename,
+    '-f', 'webm',
+    '-s', movie.width + 'x' + movie.height,
+    "-g", "30", //"-r","30", // Framerate
+    "-minrate", "1M", "-maxrate", "1M", "-b:v", "1M",
+    "-vcodec","libvpx", // H.264
+    "-acodec","libvorbis", // Vorbis
+    "pipe:1", // stdout
+  ]);
+  proc.stderr.on('data', buf => {
+    //console.log(buf);
+  });
+  proc.stdout.pipe(response);
+  response.on("close", function() {
+    proc.kill();
+  });
+}).listen(port);
+
+electron.ipcMain.on('movie', (event, arg) => {
+  let id = uuidV4();
+  streams[id] = arg;
+  let url = 'http://localhost:' + port + '/' + id;
+  console.log('Mapping ' + url + ' to \"' + arg.filename + '\"');
+  event.sender.send('movie-uri', url);
+});
