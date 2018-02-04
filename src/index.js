c4989d31f6269812af143096b183d8fe144bd1eb
Takashi Kawasaki <espresso3389@gmail.com>
2018-02-04 12:47:03 +0900

    Update code.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import electron from "electron";
 import ffmpeg from "@ffmpeg-installer/ffmpeg";
 import http from "http";
 import child_process from 'child_process';
+import path from 'path';
+import readline from "readline";
 import uuidV4 from 'uuid/v4';
 import windowStateKeeper from "electron-window-state";
 import isRunningInAsar from 'electron-is-running-in-asar';
@@ -57,12 +59,9 @@ const port = 25641;
 const movWidth = 640;
 const movHeight = 360;
 
-let streams = {};
-const guidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+let movie;
 
 http.createServer((request, response) => {
-  let possibleGuid = request.url.substr(1).toLowerCase();
-  let movie = guidRegex.test(possibleGuid) ? streams[possibleGuid] : null; // FIXME: not bad, but not enough
   if (!movie)
   {
     response.writeHead(404);
@@ -74,6 +73,7 @@ http.createServer((request, response) => {
 
   response.writeHead(200, {'Content-Type': 'video/webm'});
   const proc = child_process.spawn(ffmpeg.path, [
+    '-ss', movie.seek,
     '-i', movie.filename,
     '-s', movie.width + 'x' + movie.height,
     '-f', 'webm',
@@ -90,21 +90,61 @@ http.createServer((request, response) => {
     "-deadline", "realtime",
     "-cpu-used", String(cpuCount),
     "-threads", String(cpuCount),
-    "pipe:1", // stdout
-  ]);
-  proc.stderr.on('data', buf => {
-    //console.log(buf);
-  });
+    "-stdin",
+    '-loglevel', 'quiet',
+    "pipe:1"]);
   proc.stdout.pipe(response);
+  electron.ipcMain.on('seek', (event, seek) => {
+    try {
+      movie.seek = seek;
+      openMovie(movie);
+    } catch (e) {
+      console.log(e);
+    }
+  });
   response.on("close", function() {
     proc.kill();
   });
 }).listen(port);
 
-electron.ipcMain.on('movie', (event, arg) => {
-  let id = uuidV4();
-  streams[id] = arg;
-  let url = 'http://localhost:' + port + '/' + id;
-  console.log('Mapping ' + url + ' to \"' + arg.filename + '\"');
-  event.sender.send('movie-uri', url);
-});
+// browser accepted a movie drop
+electron.ipcMain.on('movie', (event, arg) => openMovie(arg));
+
+String.prototype.extract = function(re) {
+  const m = this.match(re);
+  if (m) return m[1];
+  return null;
+}
+
+// start playing
+function openMovie(opts) {
+  if (opts.seek == null) opts.seek = 0;
+  movie = opts;
+
+  if (movie.duration) {
+    notifyUrl();
+    return;
+  }
+
+  const proc = child_process.spawn(ffmpeg.path, ['-i', movie.filename]);
+  let fulldata = '';
+  proc.stderr.on('data', data => { fulldata += data.toString() });
+  proc.stdout.on('data', data => {});
+  proc.on('close', () => {
+    movie.created = fulldata.extract(/creation_time\s+:\s+(\d+-\d+-\d+T\d+:\d+:[\d\.]+\w+)/);
+    const m = fulldata.match(/Stream.+:.+Video:.+(yuv\w+).+, (\d{3,}x\d{3,}).+, (\d{2}\.\d+|\d{2,}) fps/);
+    if (m) {
+      movie.format = m[1];
+      movie.resolution = m[2];
+      movie.framerate = m[3];
+    }
+    movie.duration = fulldata.extract(/Duration: (\d+:\d+:\d+\.\d+),/);
+    console.log(movie);
+    notifyUrl();
+  });
+}
+
+function notifyUrl() {
+  let url = 'http://localhost:' + port + '/' + uuidV4();
+  mainWindow.webContents.send('movie-stat', { url: url, movie: movie });
+}
